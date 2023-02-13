af019a3af79d1f2d170995857d6df9db55d14806
riquew <hwindlin@gmail.com>
2023-02-13 16:28:05 -0300

    Finaliza script

--- script.js
+++ script.js
@@ -6,24 +6,47 @@
 const resetBtn = document.querySelector("[data-reset]");
 const actionBtn = document.querySelector("[data-action]");
 const visorCronometro = document.querySelector("h1.numero");
-let valorVisor = 0;
+let valorMiliSeg = 0;
 
 resetBtn.addEventListener("click", resetaTimer);
 actionBtn.addEventListener("click", verificaTimer);
 
 function resetaTimer() {
-  valorVisor = 0;
-  visorCronometro.textContent = valorVisor;
+  valorMiliSeg = 0;
+  valorInicial = "0:00:00:00";
+  visorCronometro.textContent = valorInicial;
 }
 
 function verificaTimer() {
+  let miliSeg = visorCronometro.textContent.split(":")[3];
+  let seg = visorCronometro.textContent.split(":")[2];
+  let min = visorCronometro.textContent.split(":")[1];
+  let hora = visorCronometro.textContent.split(":")[0];
+
   if (this.dataset.action === "off") {
     this.dataset.action = "on";
     const funcionaTimer = setInterval(() => {
-      valorVisor++;
-      visorCronometro.textContent = valorVisor;
+      valorMiliSeg++;
+      valorMiliSeg < 10
+        ? (miliSeg = "0" + valorMiliSeg)
+        : (miliSeg = valorMiliSeg);
+      if (valorMiliSeg > 99) {
+        valorMiliSeg = 0;
+        seg++;
+        if (seg < 10) seg = "0" + seg;
+      }
+      if (seg > 59) {
+        seg = "00";
+        min++;
+        if (min < 10) min = "0" + min;
+      }
+      if (min > 59) {
+        min = "00";
+        hora++;
+      }
+      visorCronometro.textContent = `${hora}:${min}:${seg}:${miliSeg}`;
       if (this.dataset.action === "off") clearInterval(funcionaTimer);
-    }, 1000);
+    }, 10);
   } else {
     this.dataset.action = "off";
   }
